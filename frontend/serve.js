39d11fbfe44b9dcc47d5ee9b041cabc87769abeb
Aidan Walker <aidan@aidans-mbp.home>
2021-01-06 18:57:39 +0000

    latest

--- frontend/serve.js
+++ frontend/serve.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const publicPath = path.join(__dirname, "dist");
 
 const port = process.env.PORT || 3000;
-app.use(express.static("dist"));
+app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(port);
